cff1c05594ef0d16915f82d0b44778cd7520c33a
bsautermeister <apps@bsautermeister.de>
2014-12-18 21:32:20 +0100

    Fixed Speech bug on Lumia 630-930 caused by the SpeechRecUI by updating the framework to 1.7

--- Frequenzer.App/Resources/AppResources.Designer.cs
+++ Frequenzer.App/Resources/AppResources.Designer.cs
@@ -97,7 +97,7 @@ namespace Frequenzer.App.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 1.3 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 1.4 ähnelt.
         /// </summary>
         public static string ApplicationVersion {
             get {
